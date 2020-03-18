b2d8fd929b2829b4fa403cab2685c1be5f37a56e
kkrushnyakov <github@krushnyakov.ru>
2020-03-18 06:11:19 +0300

    TestSuite

---
+++ src/test/java/ru/krushnyakov/optimaize/AllTests.java
@@ -0,0 +1,11 @@
+package ru.krushnyakov.optimaize;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DoubleCounterTest.class, IntegerCounterTest.class })
+public class AllTests {
+
+}
